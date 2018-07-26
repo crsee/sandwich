7f8e88095a6048b743165bedfd4887084b0162a5
Ezike Tobenna <tobeydaniels@gmail.com>
2018-07-27 00:47:31 +0100

    parsed JSON sandwich data in JsonUtils class, changed app color theme

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,6 +2,13 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     /**
@@ -11,11 +18,57 @@ public class JsonUtils {
      * @param json the JSON data to be parsed
      * @return Sandwich objects
      */
-
-
     public static Sandwich parseSandwichJson(String json) {
 
+        //try to parse the JSON data. if there's an error, a JSONException
+        //exception object is thrown.
+        //catch the exception so the app doesn't crash
 
-        return null;
+        try {
+            JSONObject baseJsonObject = new JSONObject(json);
+            JSONObject name = baseJsonObject.getJSONObject("name");
+
+            // sandwich name
+            String mainName = name.getString("mainName");
+
+            // sandwich alias
+            JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = parseStringArray(alsoKnownAsArray);
+
+            // sandwich place of origin
+            String origin = baseJsonObject.getString("placeOfOrigin");
+
+            // sandwich description
+            String description = baseJsonObject.getString("description");
+
+            // sandwich image
+            String image = baseJsonObject.getString("image");
+
+            // sandwich ingredient
+            JSONArray ingredientArray = baseJsonObject.getJSONArray("ingredients");
+            List<String> ingredients = parseStringArray(ingredientArray);
+
+            return new Sandwich(mainName, alsoKnownAs, origin, description, image, ingredients);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    /**
+     * @param jsonArray is the input array
+     * @return an ArrayList of Strings
+     */
+    private static List<String> parseStringArray(JSONArray jsonArray) {
+        List<String> stringArray = null;
+        if (jsonArray != null) {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                stringArray = new ArrayList<>();
+                stringArray.add(jsonArray.optString(i));
+            }
+        }
+        return stringArray;
     }
 }
