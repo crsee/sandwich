fce562f9bb271c9739cac17404bc96538394ae85
Ezike Tobenna <tobeydaniels@gmail.com>
2018-07-26 18:01:56 +0100

    updated layout

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -4,7 +4,18 @@ import com.udacity.sandwichclub.model.Sandwich;
 
 public class JsonUtils {
 
+    /**
+     * Returns a list of {@link Sandwich} objects that has been built up from
+     * parsing JSON data
+     *
+     * @param json the JSON data to be parsed
+     * @return Sandwich objects
+     */
+
+
     public static Sandwich parseSandwichJson(String json) {
+
+
         return null;
     }
 }
